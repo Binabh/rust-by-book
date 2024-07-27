567dacb5a3fc5feb2d1ed498d9812257bf8101d7
Binabh <binabhdevkota@gmail.com>
2024-07-27 12:47:07 +0545

    improve minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,42 +1,63 @@
 use std::{env, error::Error, fs, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err|{
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
-    if  let Err(e) = run(config){
+    if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
-fn run(config:  Config)->Result<(), Box<dyn Error>>{
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    let lines =  search(&config.query, &contents);
-    for line in lines{
-        println!("{}",line);
+    let lines = if config.case_insensetive {
+        search_case_insensetive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in lines {
+        println!("{}", line);
     }
     Ok(())
 }
 struct Config {
     query: String,
     file_path: String,
+    case_insensetive: bool,
 }
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not Enough Arguments");
-        }
-        let query = args[2].clone();
-        let file_path = args[1].clone();
-        Ok(Config { query, file_path })
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get file path"),
+        };
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get query string"),
+        };
+        let case_insensetive = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            case_insensetive,
+        })
     }
 }
+
+fn search_case_insensetive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|sentence| sentence.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
+}
+
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
-        .filter(|sentence| sentence.contains(query))
+        .filter(|sentence| sentence.contains(&query))
         .collect()
 }
 
@@ -45,13 +66,22 @@ mod tests {
     use super::*;
 
     #[test]
+    fn case_insensetive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["Rust:"], search_case_insensetive(query, contents));
+    }
+
+    #[test]
     fn one_result() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
-
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
